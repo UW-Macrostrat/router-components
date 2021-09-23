896dc5403331a5615b05e34c8814b29a7ad2f2c9
Daven Quinn <dev@davenquinn.com>
2021-09-23 00:24:12 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Router components
+
+Components to support the use of `react-router` and `react-router-dom` in Macrostrat projects
+with `@blueprintjs/core` UI components.
---
+++ index.ts
@@ -0,0 +1,31 @@
+import h from "@macrostrat/hyper";
+import { NavLink, withRouter, Link } from "react-router-dom";
+import { AnchorButton } from "@blueprintjs/core";
+import classNames from "classnames";
+import { LinkCard as LinkCard_ } from "@macrostrat/ui-components";
+
+// Button that forms a React Router link
+const LinkButton = withRouter(function (props) {
+  // @ts-ignore
+  let { to, history, staticContext, onClick, match, location, ...rest } = props;
+
+  onClick = function (event) {
+    if (to == null) {
+      return;
+    }
+    history.push(to);
+    return event.preventDefault();
+  };
+
+  return h(AnchorButton, { onClick, ...rest });
+});
+
+const NavLinkButton = function (props) {
+  let { className, ...rest } = props;
+  className = classNames(className, "bp3-button bp3-minimal");
+  return h(NavLink, { className, ...rest });
+};
+
+const LinkCard = (props) => h(LinkCard_, { linkComponent: Link, ...props });
+
+export { LinkButton, NavLinkButton, LinkCard };
