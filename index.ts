0b887ab9bc6c799212d443a760cf7142d1749ab6
Daven Quinn <dev@davenquinn.com>
2021-09-23 00:49:12 -0500

    Added ability to set whether NavLinkButton is minimal

--- index.ts
+++ index.ts
@@ -5,27 +5,31 @@ import classNames from "classnames";
 import { LinkCard as LinkCard_ } from "@macrostrat/ui-components";
 
 // Button that forms a React Router link
-const LinkButton = withRouter(function (props) {
-  // @ts-ignore
+const LinkButton = withRouter(function (props: any) {
   let { to, history, staticContext, onClick, match, location, ...rest } = props;
 
-  onClick = function (event) {
-    if (to == null) {
-      return;
-    }
-    history.push(to);
-    return event.preventDefault();
-  };
-
-  return h(AnchorButton, { onClick, ...rest });
+  return h(AnchorButton, {
+    onClick(event: any) {
+      if (to == null) {
+        return;
+      }
+      history.push(to);
+      event.preventDefault();
+    },
+    ...rest,
+  });
 });
 
-const NavLinkButton = function (props) {
-  let { className, ...rest } = props;
-  className = classNames(className, "bp3-button bp3-minimal");
+const NavLinkButton = function (props: any) {
+  let { className, minimal = true, large = false, ...rest } = props;
+  className = classNames(className, "bp3-button", {
+    "bp3-minimal": minimal,
+    "bp3-large": large,
+  });
   return h(NavLink, { className, ...rest });
 };
 
-const LinkCard = (props) => h(LinkCard_, { linkComponent: Link, ...props });
+const LinkCard = (props: any) =>
+  h(LinkCard_, { linkComponent: Link, ...props });
 
 export { LinkButton, NavLinkButton, LinkCard };
